fc73539539edb98364c1ea4c732875d769a9dcb2
hgfowler08 <hgfowler08@gmail.com>
2018-06-16 17:53:50 -0400

    main

---
+++ PyBank/main.py
@@ -0,0 +1,17 @@
+#Analyze financial data
+
+#Import data set
+
+#Create path to collect data from the folder
+
+#Define function and set the financial data as its parameter
+
+#Find total number of months in data set
+
+#Find net profit/loss over the time period
+
+#Find average change in profit/loss between months over the total period
+
+#Find greatest increase in profits (date and amount) over total period
+
+#Find greatest decreases in losses (date and amount) over total period
---
+++ PyPoll/main.py
@@ -0,0 +1,17 @@
+#Analyze voter data
+
+#Import data set
+
+#Create path to collect data from the folder
+
+#Define function
+
+#Find total amount of votes cast
+
+#List of all candidates
+
+#% of votes each candidate won
+
+#total number of votes each candidate won
+
+#Winner based on popular vote
