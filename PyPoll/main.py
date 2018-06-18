c3ef4a1893aa16a037caf921327d09ec01d8308b
hgfowler08 <hgfowler08@gmail.com>
2018-06-17 21:01:20 -0400

    pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,16 @@
 #Analyze voter data
+import os
+import csv
 
-#Import data set
+
 
 #Create path to collect data from the folder
-
+pollCSV = os.path.join('..', 'Resources', 'election_data.csv')
 #Define function
+def getPercentages(pollData):
 
 #Find total amount of votes cast
-
+    totalVotes = str(pollData[1])
 #List of all candidates
 
 #% of votes each candidate won
@@ -15,3 +18,16 @@
 #total number of votes each candidate won
 
 #Winner based on popular vote
+
+
+#Print values
+print("Total Votes:" + str(totalVotes))
+
+
+# Read in the CSV file
+with open(pollCSV, 'r') as csvfile:
+
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    header = next(csvreader)
