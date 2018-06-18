27d98b5cc61d800021e344897a3ee175e41f50bf
hgfowler08 <hgfowler08@gmail.com>
2018-06-17 22:35:36 -0400

    PyPoll update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,14 +3,19 @@ import os
 import csv
 
 
+#file = "election_data.csv"
 
+#Hold values for variables
+ID = [0]
+county = [0]
+candidate = [0]
 #Create path to collect data from the folder
-pollCSV = os.path.join('..', 'Resources', 'election_data.csv')
+#pollCSV = os.path.join('..', 'Resources', 'election_data.csv')
 #Define function
-def getPercentages(pollData):
+#def getPercentages(pollData):
 
 #Find total amount of votes cast
-    totalVotes = str(pollData[1])
+    #totalVotes = int(pollData[1])
 #List of all candidates
 
 #% of votes each candidate won
@@ -21,13 +26,21 @@ def getPercentages(pollData):
 
 
 #Print values
-print("Total Votes:" + str(totalVotes))
+#print("Total Votes:" + str(totalVotes))
 
 
 # Read in the CSV file
-with open(pollCSV, 'r') as csvfile:
+with open('election_data.csv', 'r') as csvfile:
 
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
 
     header = next(csvreader)
+
+    for row in csvreader:
+        if row[0] == voter:
+            print(voter)
+        elif row[1] == county:
+            print(county)
+        elif row[2] == candidate:
+            print(candidate)
