9a63430b57f37566b4df7840c23676a204a7e599
hgfowler08 <hgfowler08@gmail.com>
2018-06-19 11:16:10 -0400

    Update

--- PyBank/main.py
+++ PyBank/main.py
@@ -48,23 +48,28 @@ with open(file, 'r') as f:
     
     
 #Find greatest increase in profits (date and amount) over total period
-    #greatestincrease = (revenue)
-    #for x in range(0, len(revenue)):
-        #greatestincrease = revenue[x + 1] - revenue[x]
+    greatestincrease = int()
+    for x in range(len(revenue)):
+        revenue[x] = int(revenue[x])
+        greatestincrease = revenue[x] + revenue[x]
     
 
 #Find greatest decreases in losses (date and amount) over total period
-
+    greatestdecrease = int()
+    for x in range(len(revenue)):
+        revenue[x] = int(revenue[x])
+        greatestdecrease = revenue[x] - revenue[x]
 
 
 print("Total Months:" + str(totalMonths))
 print("Total:" + str(netRev))
 print("Average Change:" + str(totalChange))
-#print("Greatest Increase in Profits:" + date + str(greatestincrease))
+print("Greatest Increase in Profits:" + str(date) + str(greatestincrease))
+print("Greatest Decrease in Profits:" + str(date) + str(greatestdecrease))
 
 output = "pybank.csv"
 
-with open('output', 'w', newline='') as csvfile:
+with open('output','w', newline='') as csvfile:
     writer = csv.writer(csvfile)
 
     writer.writerow(["Financial Analysis"])
@@ -72,5 +77,6 @@ with open('output', 'w', newline='') as csvfile:
     writer.writerow(["Total Months:" + str(totalMonths)])
     writer.writerow(["Total:" + str(netRev)])
     writer.writerow(["Average Change:" + str(totalChange)])
-    #writer.writerow(["Greatest Increase in Profits:", + date + str(greatestincrease)])
+    writer.writerow(["Greatest Increase in Profits:" + str(date) + str(greatestincrease)])
+    writer.writerow(["Greatest Decrease in Profits:" + str(date) + str(greatestdecrease)])
 
