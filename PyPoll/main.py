46d897becd3fac2d1a43c57dc33af67da6c1e2ab
hgfowler08 <hgfowler08@gmail.com>
2018-06-18 20:33:09 -0400

    update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,12 @@
 #Analyze voter data
-import os
+#import os
 import csv
 
 
-#file = "election_data.csv"
+file = 'C:\\Users\\hgfow\\Desktop\\python-challenge\\PyPoll\\election_data.csv'
 
 #Hold values for variables
-ID = [0]
+voter = [0]
 county = [0]
 candidate = [0]
 #Create path to collect data from the folder
@@ -14,8 +14,21 @@ candidate = [0]
 #Define function
 #def getPercentages(pollData):
 
+# Read in the CSV file
+with open('election_data.csv', 'r') as f:
+
+    # Split the data on commas
+    csvreader = csv.reader(f, delimiter=',')
+
+    header = next(csvreader)
+
+    for row in csvreader:
+        voter.append(row[0])
+        county.append(row[1])
+        candidate.append(row[2])
+
 #Find total amount of votes cast
-    #totalVotes = int(pollData[1])
+        voter = len(row[0])    
 #List of all candidates
 
 #% of votes each candidate won
@@ -26,21 +39,6 @@ candidate = [0]
 
 
 #Print values
-#print("Total Votes:" + str(totalVotes))
+print("Total Votes:" + str(voter))
 
 
-# Read in the CSV file
-with open('election_data.csv', 'r') as csvfile:
-
-    # Split the data on commas
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-    header = next(csvreader)
-
-    for row in csvreader:
-        if row[0] == voter:
-            print(voter)
-        elif row[1] == county:
-            print(county)
-        elif row[2] == candidate:
-            print(candidate)
